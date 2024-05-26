e02353983b0cfeb5437d529cbc4128f682de505d
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-26 10:47:44 +0700

    create .env and configured next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	reactStrictMode: false,
+	poweredByHeader: false,
+	optimizeFonts: false,
+	env: {
+		SERVER_URL: process.env.REACT_SERVER_URL,
+	},
+}
 
-export default nextConfig;
+export default nextConfig
