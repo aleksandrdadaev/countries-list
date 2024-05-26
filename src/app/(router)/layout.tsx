84c3106a45e39e26ccba675b355e19adacd4c00c
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-26 10:43:30 +0700

    clear main page and layout

---
+++ src/app/(router)/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+	weight: ['300', '400', '500', '600', '700'],
+	style: 'normal',
+	variable: '--font-inter',
+	subsets: ['cyrillic', 'latin'],
+	display: 'swap',
+})
+
+export const metadata: Metadata = {
+	title: 'Countries List',
+}
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<html
+			lang='en'
+			style={{
+				fontFamily: inter.style.fontFamily,
+			}}
+		>
+			<body>{children}</body>
+		</html>
+	)
+}
