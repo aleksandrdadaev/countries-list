2a2f91a58a4dad0b5e4f933d5273b57c22af1c6a
Sanchezz <sasha.dadaev.98@mail.ru>
2024-05-27 11:51:21 +0700

    create get all countries hook

--- src/features/get-all-countries/lib/hooks/useGetAllCountries.ts
+++ src/features/get-all-countries/lib/hooks/useGetAllCountries.ts
@@ -1,1 +1,18 @@
-export const useGetAllCountries = () => {}
+import { useQuery } from '@tanstack/react-query'
+
+import { CountryService } from '@/shared/api/services/country.service'
+
+export const useGetAllCountries = () => {
+	const { data, isError, isLoading, isSuccess } = useQuery({
+		queryKey: ['get all countries'],
+		queryFn: () => CountryService.getAll(),
+		select: data => data.data,
+	})
+
+	return {
+		countries: data,
+		isError,
+		isLoading,
+		isSuccess,
+	}
+}
